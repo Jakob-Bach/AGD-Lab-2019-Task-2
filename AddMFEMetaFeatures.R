4eefd21b286d90c9be8f305823ca6e262bdb86fd
Jakob Bach <jakob.bach@kit.edu>
2019-07-02 09:58:47 +0200

    Fix variable naming in mfe extraction

--- AddMFEMetaFeatures.R
+++ AddMFEMetaFeatures.R
@@ -11,15 +11,15 @@ OpenML::setOMLConfig(apikey = "c1994bdb7ecb3c6f3c8f3b35f4b47f1f") # read-only de
 datasetIds <- fread(MAIN_IN_FILE)[, unique(get(ID_COL))]
 
 cat("Extracting meta-features with mfe...\n")
-progressBar <- txtProgressBar(min = 0, max = length(dataIds), style = 3)
+progressBar <- txtProgressBar(min = 0, max = length(datasetIds), style = 3)
 library(foreach)
 showProgress <- function(n) setTxtProgressBar(progressBar, n)
 computingCluster <- parallel::makeCluster(parallel::detectCores())
 doSNOW::registerDoSNOW(computingCluster)
-mfeTable <- rbindlist(foreach(i = 1:length(dataIds), .packages = "OpenML",
+mfeTable <- rbindlist(foreach(i = 1:length(datasetIds), .packages = "OpenML",
     .options.snow = list(progress = showProgress)) %dopar% {
   setTxtProgressBar(progressBar, value = i)
-  dataList <- OpenML::getOMLDataSet(data.id = dataIds[i])
+  dataList <- OpenML::getOMLDataSet(data.id = datasetIds[i])
   dataset <- dataList$data
   targetColumn <- dataList$target.features
   dataset <- data.frame(lapply(dataset, function(col) {
@@ -42,7 +42,7 @@ mfeTable <- rbindlist(foreach(i = 1:length(dataIds), .packages = "OpenML",
     groups = c("general", "infotheo", "statistical")
   )
   result <- list()
-  result[[ID_COL]] <- dataIds[i]
+  result[[ID_COL]] <- datasetIds[i]
   result[names(metaFeatureVec)] <- metaFeatureVec
   return(result)
 })
